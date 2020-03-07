74c74f02ed5ff7a0b3d1c65ed509eb7187c69664
Kogan <koganp42@gmail.com>
2020-03-07 17:40:31 -0600

    Updated server to require api route correctly

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
+const PORT = process.env.PORT || 8080;
 
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('combined'));
 
 app.use(express.static("public"));
 require("./routes/html-routes")(app);
-const router = require("./routes/api-routes");
+require("./routes/api-routes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
