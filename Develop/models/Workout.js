e7d62ccaba3ac41efda99d8ab81284be6434da77
Kogan <koganp42@gmail.com>
2020-03-07 17:13:05 -0600

    Switched to one model

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const opts = {toJSON: {virtuals: true}};
 
 const WorkoutSchema = new Schema({
   day: {
-    type: Date
+    type: Date,
+    default: Date.now
   },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
-    }
-  ]
-});
+  exercises:[]
+}, opts);
 
+WorkoutSchema.virtual("totalDuration").get(function(){
+  let totalDuration = 0;
+  for (const exercise of this.exercises){
+    totalDuration += exercise.duration
+  }
+  return totalDuration
+})
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
