5386b26d7ea1c2d820686a59805214158398c0c5
Kogan <koganp42@gmail.com>
2020-03-07 17:58:44 -0600

    Configured mongodb env variable

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('combined'));
-
 app.use(express.static("public"));
+
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  userFindAndModify: false
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
